42a477e72d3b839d1e3b44ea56abe9f78c2a1429
alfan2911 <alfansyuriziaulhaq11@gmail.com>
2024-01-02 09:47:10 +0700

    first-commit

---
+++ src/Components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './MovieDetails.css'
+import { MovieDetail } from '../../api'
+
+const MovieDetails = () => {
+  return 
+  return (
+    <div className='movie-details'>
+      <h3>Title :{movie.title}</h3>
+      <p>Details :</p>
+
+    </div>
+  )
+}
+
+export default MovieDetails
---
+++ src/Pages/Details.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MovieDetails from '../Components/MovieDetails/MovieDetails'
+
+
+const Details = () => {
+  return (
+    <div>
+      <MovieDetails/>
+    </div>
+  )
+}
+
+export default Details
+
